93adfec55407eb092144bd51a224dc31e25525e3
Kakomanato <160876603+Kakomanato@users.noreply.github.com>
2024-05-11 01:38:45 +0000

    made changes to index.html, style.css and app.js to develop the exercise

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardValue = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const cardType = ["♦", "♥", "♠", "♣"];
+
+  const randomValueIndex = Math.floor(Math.random() * cardValue.length);
+  const randomTypeIndex = Math.floor(Math.random() * cardType.length);
+
+  const randomCardValue = cardValue[randomValueIndex];
+  const randomCardType = cardType[randomTypeIndex];
+
+  const displayUp = document.querySelector(".card-symbol-up");
+  const displayDown = document.querySelector(".card-symbol-down");
+  const displayNumber = document.querySelector(".card-number");
+
+  displayUp.innerHTML = randomCardType;
+  displayDown.innerHTML = randomCardType;
+  displayNumber.innerHTML = randomCardValue;
+
+  const isRed = randomCardType === "♥" || randomCardType === "♦";
+
+  if (isRed) {
+    displayUp.classList.add("red-text");
+    displayDown.classList.add("red-text");
+    displayNumber.classList.add("red-text");
+  }
 };
